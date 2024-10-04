49d75e921ae61508c3f1890a3e50a61c2ab6a686
maiabdallah7 <maiabdalaa2019@gmail.com>
2024-10-04 08:21:41 +0300

    prepare the project

---
+++ index.js
@@ -0,0 +1,14 @@
+class Student{
+    name
+    age
+    phone
+}
+
+let Student1 = new Student();
+Student1.name = 'Mai'
+Student1.age = 22
+Student1.phone = 1050226242
+
+console.log(Student1
+    
+)
